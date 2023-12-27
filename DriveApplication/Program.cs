69b5ce3071dc288192ded938eb6140c4ec1cbbd6
Ayush Singh <ayush6203@gmail.com>
2023-12-27 11:57:50 +0530

    Final commit of snake ladder game

--- DriveApplication/Program.cs
+++ DriveApplication/Program.cs
@@ -1,13 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using SnakeAndLadderImplementation.Interfaces;
 using SnakeAndLadderImplementation.Models;
 
+#region SnakeandLadder Game
 Console.WriteLine("Snake and ladder game");
-List<IJump> snakes = new List<IJump>() { new Snake() { Start = 99, End = 9}, new Snake() { Start = 45, End = 15 }, new Snake() { Start = 17, End = 3 }, new Snake() { Start = 63, End = 40 } };
-List<IJump> ladder = new List<IJump>() { new Ladder() { Start = 7, End = 54 }, new Ladder() { Start = 66, End = 83 }, new Ladder() { Start = 27, End = 33 }, new Ladder() { Start = 69, End = 91 } };
-List<Player> players = new List<Player>() { new Player("Ayush"), new Player("Piyush") };
-Board board = new Board(10, snakes, ladder, players);
+List<Player> players = new List<Player>() { new Player("Ayush"), new Player("Piyush"), new Player("Arsh") };
+Board board = new Board(10, 5, 5, players);
 board.BeginGamePlay();
+#endregion
 
 
 Console.ReadKey();
